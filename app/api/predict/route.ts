bdddb35635214b5f853feaa664e627e6b0dd453f
sneax <paladesh600@gmail.com>
2025-06-27 00:28:12 +0530

    minor-change

--- app/api/predict/route.ts
+++ app/api/predict/route.ts
@@ -4,6 +4,7 @@ import { type NextRequest, NextResponse } from 'next/server';
 export async function POST(request: NextRequest) {
   const formData = await request.formData();
   const file = formData.get('file') as File;
+  formData.append("audio_file", file);
 
   if (!file) {
     return NextResponse.json({ error: 'No file provided' }, { status: 400 });
